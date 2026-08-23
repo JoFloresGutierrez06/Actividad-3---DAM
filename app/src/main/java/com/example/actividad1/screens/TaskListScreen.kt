ef469dad766c42684412e2a902dbfe2140811123
Jocelyn Flores <jofloresgutierrez@gmail.com>
2026-08-23 17:28:53 -0600

    División del código que estaba todo en el MainActivity en varios archivos

---
+++ app/src/main/java/com/example/actividad1/screens/TaskListScreen.kt
@@ -0,0 +1,52 @@
+package com.example.actividad1.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.example.actividad1.Task
+
+@Composable
+fun TaskListScreen(
+    tasks: List<Task>,
+    onCompletedChange: (Task, Boolean) -> Unit // "MainActivity, tú tienes los datos. Yo solamente te aviso cuando el usuario haga algo."
+) {
+
+    Column {
+
+        Text("Mis tareas")
+
+        LazyColumn {
+
+            items(tasks) { task ->
+
+                TaskItem(
+                    task = task,
+                    onCompletedChange = { completed ->
+                        onCompletedChange(task, completed)
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskItem(
+    task: Task,
+    onCompletedChange: (Boolean) -> Unit
+) {
+
+    Row {
+
+        Checkbox(
+            checked = task.completed,
+            onCheckedChange = onCompletedChange
+        )
+
+        Text(task.title)
+    }
+}
